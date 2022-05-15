644255bb5611a802637df2dee016924d722ef329
Jeremiah <jaoluborode@student.lautech.edu.ng>
2022-05-15 16:24:26 +0530

    Made a change to the login page

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -78,13 +78,9 @@ WSGI_APPLICATION = 'TodoApp.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "TODOAPPLICATION",
-        "USER": "root",
-        "PASSWORD" : "",
-        "HOST" : "localhost",
-        "PORT" : "3306",
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
